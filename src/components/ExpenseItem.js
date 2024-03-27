9285866c07b7947d07c9d129706fa3f6b7673ffb
Alex Wong <wallcealexwong@hotmail.com>
2024-03-27 15:16:30 -0400

    Add plus and minus button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
-//import { FaPlusCircle } from 'react-icons/fa';
+import { FaPlusCircle, FaMinusCircle } from 'react-icons/fa';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -42,10 +42,9 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
-        <td><FaPlusCircle size='1.5em' onClick={event=> increaseAllocation(props.name)}></FaPlusCircle></td>
-
-        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        <td><FaPlusCircle size='1.5em' color='green' onClick={event=> increaseAllocation(props.name)}></FaPlusCircle></td>
+        <td><FaMinusCircle size='1.5em' color='red' onClick={event=> decreaseAllocation(props.name)}></FaMinusCircle></td>
+        <td><TiDelete size='1.5em' color='black' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
